f7769e6489125f8a1e04004a963e198713605fce
xema4789 <jmqmaestre@gmail.com>
2020-03-19 12:51:45 +0100

    Iniciando_login

--- module/login/controller/controller_login.php
+++ module/login/controller/controller_login.php
@@ -1,6 +1,5 @@
 <?php
- $path = $_SERVER['DOCUMENT_ROOT'] . '/Programacion/Tema5_1.0/Tema5_1.0/8_MVC_CRUD/'; //no me esta pillando bien el path y en consecuencia el include de despues
- include($path . "/module/login/model/DAOLogin.php");
+ $path = $_SERVER['DOCUMENT_ROOT'] . '/Programacion/Tema5_1.0/Tema5_1.0/8_MVC_CRUD/'; 
 
  switch($_GET['op']){
    case 'list_login':
